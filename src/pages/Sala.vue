109693ccc720b163501af6950acd2723854c61c2
Maxleandro14 <maxleandro2@gmail.com>
2020-07-16 10:37:16 -0300

    atualiizar

--- src/pages/Sala.vue
+++ src/pages/Sala.vue
@@ -1,5 +1,26 @@
 <template>
   <div>
+    <div class="q-pa-md" style="max-width: 350px">
+    <q-list padding bordered class="rounded-borders">
+
+      <q-expansion-item
+        dense
+        dense-toggle
+        expand-separator
+        icon="drafts"
+        label="Publicação fixada"
+        header-class="text-purple"
+      >
+        <q-card>
+          <q-card-section>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
+            commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
+            eveniet doloribus ullam aliquid.
+          </q-card-section>
+        </q-card>
+      </q-expansion-item>
+    </q-list>
+  </div>
     <div class="row  flex-center">
 
       <div class="col text-center">
@@ -42,8 +63,9 @@
 <q-input v-model="paginaAtual" :dense="dense" />
 </q-field>
 
-<q-btn flat round color="secondary" icon="exposure_plus_2" />
 <q-btn flat round color="secondary" icon="exposure_neg_1" />
+<q-btn flat round color="secondary" icon="exposure_plus_1" />
+<q-btn flat round color="secondary" icon="exposure_plus_2" />
 
 </div>
 
