0d5fa3b9892a62b6d103d25e73db6311d91de772
Maxleandro14 <maxleandro2@gmail.com>
2020-07-24 14:11:50 -0300

    atualizarrrr

--- src/pages/Sala.vue
+++ src/pages/Sala.vue
@@ -7,15 +7,16 @@
         dense
         dense-toggle
         expand-separator
-        icon="drafts"
-        label="Publicação fixada"
+        icon="campaign"
+        label="Debate"
         header-class="text-purple"
       >
         <q-card>
           <q-card-section>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
-            commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
-            eveniet doloribus ullam aliquid.
+            2/22/2020: 300 páginas<br />
+            2/22/2020: 300 páginas<br />
+            2/22/2020: 300 páginas<br />
+            2/22/2020: 300 páginas<br />
           </q-card-section>
         </q-card>
       </q-expansion-item>
@@ -75,7 +76,7 @@
 <div class="row flex-center emoji-book ">
 
     <label>
-      <input type="radio" name="emoji" value="feliz" checked>
+      <input type="radio" v-model="emoji" value="Alegre" label="Alegre">
 
       <svg viewBox="0 0 24 24">
         <path fill="currentColor" d="M12,2C6.47,2 2,6.47 2,12C2,17.53 6.47,22 12,22A10,10 0 0,0 22,12C22,6.47 17.5,2 12,2M8.88,7.82L11,9.94L9.94,11L8.88,9.94L7.82,11L6.76,9.94L8.88,7.82M12,17.5C9.67,17.5 7.69,16.04 6.89,14H17.11C16.31,16.04 14.33,17.5 12,17.5M16.18,11L15.12,9.94L14.06,11L13,9.94L15.12,7.82L17.24,9.94L16.18,11Z" />
@@ -84,7 +85,7 @@
     </label>
 
      <label>
-      <input type="radio" name="emoji" value="rindo">
+      <input type="radio" v-model="emoji" value="Divertido" label="Divertido">
 
       <svg viewBox="0 0 24 24">
         <path fill="currentColor" d="M15.07 8.93V4.93A4.06 4.06 0 0 1 15.73 2.72A10 10 0 0 0 2.73 15.72A4.06 4.06 0 0 1 4.94 15.06H8.94V19.06A4.06 4.06 0 0 1 8.28 21.27A10 10 0 0 0 21.28 8.27A4.06 4.06 0 0 1 19.07 8.93M11 6H12.5V7.5H14V9H11M7.5 14V12.5H6V11H9V14M15.89 15.9A5.5 5.5 0 0 1 9.8 17L17 9.8A5.5 5.5 0 0 1 15.89 15.89M4.89 21.08A2 2 0 0 1 2.89 19.08A2 2 0 0 1 4.89 17.08H6.89V19.08A2 2 0 0 1 4.93 21.07M19.07 2.93A2 2 0 0 1 21.07 4.93A2 2 0 0 1 19.07 6.93H17.07V4.93A2 2 0 0 1 19.07 2.93Z" />
@@ -93,7 +94,7 @@
     </label>
 
     <label>
-      <input type="radio" name="emoji" value="chorando">
+      <input type="radio" v-model="emoji" value="Triste" label="Triste">
 
       <svg viewBox="0 0 24 24">
         <path fill="currentColor" d="M5.14,17.57C5.14,16.5 6.32,14.5 7.57,12.81C8.82,14.5 10,16.5 10,17.57A2.43,2.43 0 0,1 7.57,20C6.23,20 5.14,18.91 5.14,17.57M22,12A10,10 0 0,1 12,22C11.08,22 10.18,21.86 9.33,21.63C10.9,20.95 12,19.39 12,17.57C12,17.12 11.89,16.6 11.69,16C11.79,16 11.89,16 12,16C13.25,16 14.32,16.5 14.77,17.23L16.19,15.81C15.29,14.72 13.75,14 12,14C11.59,14 11.19,14.04 10.81,14.12C10.38,13.36 9.85,12.53 9.19,11.63L8.71,11C9.42,10.87 10,10.23 10,9.5C10,8.7 9.3,8 8.5,8C7.7,8 7,8.7 7,9.5C7,9.69 7.04,9.87 7.11,10.04L5.96,11.63C4.4,13.75 3.5,15.5 3.23,16.81C2.45,15.38 2,13.74 2,12A10,10 0 0,1 12,2A10,10 0 0,1 22,12M17,9.5C17,8.7 16.3,8 15.5,8C14.7,8 14,8.7 14,9.5C14,10.3 14.7,11 15.5,11C16.3,11 17,10.3 17,9.5Z" />
@@ -102,7 +103,7 @@
     </label>
 
     <label>
-      <input type="radio" name="emoji" value="irrelevante">
+      <input type="radio" v-model="emoji" value="Irrelevante" label="Irrelevante">
 
       <svg viewBox="0 0 24 24">
         <path fill="currentColor" d="M12 2A10 10 0 1 0 22 12A10 10 0 0 0 12 2M8.5 8A1.5 1.5 0 1 1 7 9.5A1.54 1.54 0 0 1 8.5 8M17 16H13A4 4 0 0 0 9.53 18L7.8 17A6 6 0 0 1 13 14H17M15.5 11A1.5 1.5 0 1 1 17 9.5A1.54 1.54 0 0 1 15.5 11Z" />
@@ -111,13 +112,15 @@
     </label>
 
       <label>
-      <input type="radio" name="emoji" value="raiva">
+      <input type="radio" v-model="emoji" value="Ruim" label="Ruim">
 
     <svg viewBox="0 0 24 24">
         <path fill="currentColor" d="M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M7,9.5V8L10,9.5C10,10.3 9.3,11 8.5,11C7.7,11 7,10.3 7,9.5M14.77,17.23C14.32,16.5 13.25,16 12,16C10.75,16 9.68,16.5 9.23,17.23L7.81,15.81C8.71,14.72 10.25,14 12,14C13.75,14 15.29,14.72 16.19,15.81L14.77,17.23M17,9.5C17,10.3 16.3,11 15.5,11C14.7,11 14,10.3 14,9.5L17,8V9.5Z" />
     </svg>
 
     </label>
+
+    <div style="color: #9c27b0; font-size: 17px; height: 23px;">{{emoji}}</div>
 
 </div>
 
@@ -209,6 +212,7 @@ export default {
     return {
       persistent: false,
       value: 81,
+      emoji: 'Opção',
       fab2: false,
       dense: false,
       paginaAtual: 224,
